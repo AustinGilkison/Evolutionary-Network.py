fab22746c38d208b24254a60567195af4bcbfed5
Austin Gilkison <austingilkison98@gmail.com>
2018-03-17 07:22:18 -0400

    Added basicAPI to Core.py, also created a unit test for Core.py

--- Core.py
+++ Core.py
@@ -1,93 +1,48 @@
 
 import Population, string, time, Lib
 
-target = "cat in the hat"
-mutationRate = 1
-popmax = 2000
-Accepted = string.ascii_letters + " "
 
-
-population_ = Population.population(target, mutationRate, popmax, Accepted)
-
-index = 1
-highest = 0
-while highest < 100:
-    #print(index)
-    #print("Starting main fitnesscal")
-    population_.calcFitness()
-    #print("Finished main fitnesscal")
-
-    #print("Starting natural section")
-    population_.naturalSelection()
-    #print("Finishing natural section")
-
-    #print("Generating next Population")
-    population_.generate()
-    #print("Finished generating next Population")
-
-    #print("Evaluating population")
-    loop = int(population_.evaluate()*100)
-
-    MostFit = population_.fitestElement()
-
-    print("Fitest from population " + str(index) + ' is "' + str(MostFit) + '" with a fitness of ' + str(loop))
-    if loop > highest:
-        highest = loop
-    print("Evaluation "+ str(highest)) 
-    #print(" ")
-    index = index + 1
-
-
-class sim:
+class engine:
 
     def __init__(self, target, mutationRate, popmax):
-
         self.target = target
         self.mutationRate = mutationRate
         self.popmax = popmax
-        self.Accepted = string.ascii_letters + " "
-        #print("starting pop")
-        self.population_ = Population.population(self.target, self.mutationRate, self.popmax, self.Accepted)
-        #print("pop finished")
-    #@Lib.time_it
-    def Simulate(self):
+        self.accepted = string.ascii_letters + " "
+
+    def runSimulation(self):
+    
+        population = Population.population(self.target, self.mutationRate, self.popmax, self.accepted)
+
         index = 1
         highest = 0
         while highest < 100:
             #print(index)
             #print("Starting main fitnesscal")
-            self.population_.calcFitness()
+            population.calcFitness()
             #print("Finished main fitnesscal")
 
             #print("Starting natural section")
-            self.population_.naturalSelection()
+            population.naturalSelection()
             #print("Finishing natural section")
 
             #print("Generating next Population")
-            self.population_.generate()
+            population.generate()
             #print("Finished generating next Population")
 
             #print("Evaluating population")
-            loop = int(self.population_.evaluate()*100)
+            loop = int(population.evaluate()*100)
 
-            MostFit = self.population_.fitestElement()
+            MostFit = population.fitestElement()
 
-            #print("Fitest from population " + str(index) + ' is "' + str(MostFit) + '" with a fitness of ' + str(loop))
+            print("Fitest from population " + str(index) + ' is "' + str(MostFit) + '" with a fitness of ' + str(loop) + "%")
             if loop > highest:
                 highest = loop
             #print("Evaluation "+ str(highest)) 
             #print(" ")
             index = index + 1
-        
-        '''
-        def test():
-            dna = population_.population[0]
-            pop = dna.genes
-            for j in population_.population:
-                print(" ")
-                for i in pop:
-                    print(i)
-                    '''
+
+    
 def time_it(func):
         def wrapper(*args, **kwargs):
             start = time.time()
---
+++ Test.py
@@ -0,0 +1,30 @@
+import Core
+
+instances = []
+test_phrase = "This is a test"
+mutation_rate = 1
+population_number = 500
+
+
+def createInstances(TotalinstanceNumber):
+    try:
+        for i in range(0, TotalinstanceNumber):
+            instances.append(Core.engine(test_phrase, mutation_rate, population_number))
+        print("Build finished")
+    except Exception as ext:
+        print("Build failed: " +  str(ext) )
+        exit()
+def testAttributes():
+    if(
+        instances[0].target == test_phrase and 
+        instances[0].mutationRate == mutation_rate and
+        instances[0].popmax == population_number
+        ):
+        print("testAttributes finished")
+    else:
+        print("testAttributes failed")
+        exit()
+
+
+createInstances(10)
+testAttributes()
